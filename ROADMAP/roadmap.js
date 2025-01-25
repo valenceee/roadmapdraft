08dd4a4027a8091a4da3b78668799b58c5e0519e
Carmen Rianna <150813207+valenceee@users.noreply.github.com>
2025-01-25 19:52:56 +0800

    Add files via upload

--- ROADMAP/roadmap.js
+++ ROADMAP/roadmap.js
@@ -6,23 +6,41 @@ navButton.addEventListener('click', () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const aboutITItem = document.querySelector(".main_nav ul li:first-child"); 
+    const aboutITItem = document.querySelector(".main_nav ul li:nth-child(1)"); 
     const subjectsItem = document.querySelector(".main_nav ul li:nth-child(2)"); 
+    const activitiesItem = document.querySelector(".main_nav ul li:nth-child(3)"); 
+
     const imgHome = document.querySelector(".img-home");
     const imgSub1 = document.querySelector(".img-sub1");
     const imgSub2 = document.querySelector(".img-sub2");
+    const imgActivities1 = document.querySelector(".img-activities1");
+    const imgActivities2 = document.querySelector(".img-activities2");
 
-    // When the second <li> (subjects) is clicked
-    subjectsItem.addEventListener("click", () => {
-        imgHome.style.display = "none"; 
-        imgSub1.style.display = "block";
-        imgSub2.style.display = "block"; 
-    });
+    const hideAllImages = () => {
+        imgHome.style.display = "none";
+        imgSub1.style.display = "none";
+        imgSub2.style.display = "none";
+        imgActivities1.style.display = "none";
+        imgActivities2.style.display = "none";
+    };
 
-    // When the first <li> (about IT) is clicked
     aboutITItem.addEventListener("click", () => {
+        hideAllImages();
         imgHome.style.display = "block"; 
-        imgSub1.style.display = "none"; 
-        imgSub2.style.display = "none"; 
     });
+
+    subjectsItem.addEventListener("click", () => {
+        hideAllImages();
+        imgSub1.style.display = "block"; 
+        imgSub2.style.display = "block";
+    });
+
+    activitiesItem.addEventListener("click", () => {
+        hideAllImages();
+        imgActivities1.style.display = "block"; 
+        imgActivities2.style.display = "block";
+    });
+
+    hideAllImages();
+    imgHome.style.display = "block"; 
 });
